844bec18ada19bc8e294f6bae092b28cc17d1ee3
narizzang <hr_baby@naver.com>
2025-02-25 20:20:21 +0000

    create detail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Button, Nav, Container, Navbar, Row, Col } from "react-bootstrap";
-import data from './data.js'
-
+import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
-  let [shoes] = useState(data)
+  let [shoes] = useState(data);
 
   return (
     <div className="App">
@@ -22,37 +21,44 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-      <Container>
-        <Row>
-          <Col xs={12} md={4}>
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="80%"
-            />
-            <h4>{ shoes[0].title }</h4>
-            <p>{ shoes[0].content }</p>
-            <p>{ shoes[0].price }</p>
-          </Col>
-          <Col xs={12} md={4}>
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col xs={12} md={4}>
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="80%"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-        </Row>
-      </Container>
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {shoes.map(function (shoe, i) {
+                    return <Card item={shoes[i]} i={i} />;
+                  })}
+                </Row>
+              </Container>
+            </>
+          }
+        />
+        <Route path="/detail" element={<div>어바웃페이지임</div>} />
+      </Routes>
     </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <Col xs={12} md={4}>
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.item.title}</h4>
+      <p>{props.item.content}</p>
+      <p>{props.item.price}</p>
+    </Col>
   );
 }
 
